54f2e71fb3ba432dd678864ba976aa49ffaf36b5
Sahin Cakrak <sahinc8@gmail.com>
2024-11-23 01:14:56 +0100

    fix E_8b fatal error

--- Exercise_3/E8_b.cpp
+++ Exercise_3/E8_b.cpp
@@ -3,25 +3,25 @@
 #include <string.h>
 
 
-int main(){
+int main(int argc, char **argv){
 
-    char[] message = "Hello world";
+    
 
     MPI_Init(&argc, &argv);
-
-    int mpi_size, mpi_rank;
+    const char message[] = "Hello world";
+    int mpi_size, mpi_rank, i;
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size); // Get total number of processes
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank); // Get rank of current process
     if(mpi_rank == 0){
-        for(int i=1; i<=mpi_size; i++)
+        for(int i=1; i<mpi_size; i++)
         MPI_Send(&message, strlen(message)+1, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-        printf("Rank 0 sent the message %d to rank %d\n", message, i);
+        std::cout<<"Rank 0 send the message: "<<message<<"to rank "<<i<<std::endl;
 
     }
     else{
-        char[20] received;
-        MPI_Recv(received, 20, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Rank %d received the message: '%d' from rank 0\n", mpi_rank, received);
+        char received[20];
+        MPI_Recv(&received, 20, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::cout<<"Rank "<<mpi_rank<<" received the message: "<<received<<std::endl;
 
     }
 
